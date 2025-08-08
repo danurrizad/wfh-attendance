87f6e3dcd173ce002e18e1d152f4afa59370fa48
danurrizad <danurrizad242@gmail.com>
2025-08-09 01:06:33 +0700

    fix: style width layout

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet,  } from "react-router";
-import Sidebar from "../components/UI/Sidebar";
-import Header from "../components/UI/Header";
+import Sidebar from "../components/ui/Sidebar";
+import Header from "../components/ui/Header";
 
 const LayoutContent = () => {
   return (
@@ -12,7 +12,7 @@ const LayoutContent = () => {
         className={`flex-1 transition-all duration-300 ease-in-out  bg-slate-100 `}
       >
         <Header />
-        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6 mt-20 ml-50 ">
+        <div className="p-4 mx-auto  md:p-6 mt-20 ml-50 ">
           <Outlet/>
         </div>
       </div>
