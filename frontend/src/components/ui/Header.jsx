46c0e86e4c9b47ad777d7ef7c9e88314b1c9d69d
danurrizad <danurrizad242@gmail.com>
2025-08-09 01:05:04 +0700

    update: add user's name and role, and update button logout

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import useAuthService from '../../services/AuthService'
 import { useNavigate } from 'react-router'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faSignOut, faUserCircle } from "@fortawesome/free-solid-svg-icons"
+import useVerify from "../../hooks/useVerify"
+import Button from "../../components/forms/Button"
 
 const Header = () => {
+  const { auth, clearAuth } = useVerify()
   const { logout } = useAuthService()
   const navigate = useNavigate()
 
@@ -13,18 +16,27 @@ const Header = () => {
       await logout()
       localStorage.setItem("accessToken", "")
       localStorage.setItem("refreshToken", "")
+      clearAuth()
       navigate("/login")
     } catch (error) {
       console.error(error)
     }
   }
+
   return (
-    <div className='w-screen fixed top-0 z-0 flex justify-between items-center p-4 bg-white h-[100px] border-b-1 border-gray-300'>
-      <div className='ml-[200px]'>
+    <div className='w-screen fixed top-0 z-0 flex justify-between items-center p-4 px-10 bg-white h-[100px] border-b-1 border-gray-300'>
+      <div className='ml-[190px] flex items-center gap-4'>
         <button className='cursor-pointer'><FontAwesomeIcon icon={faBars} /></button>
+        <div className='flex items-center gap-2'>
+          <FontAwesomeIcon icon={faUserCircle} className='text-5xl'/>
+          <div>
+            <p>{auth.name}</p>
+            <p>{auth.roleName}</p>
+          </div>
+        </div>
       </div>
       <div>
-        <button onClick={handleLogout} className='bg-red-400 px-10 py-1 rounded-md text-white hover:bg-red-600'>Logout</button>
+        <Button variant='outline' startIcon={<FontAwesomeIcon icon={faSignOut}/>} onClick={handleLogout}>Logout</Button>
       </div>
     </div>
   )
