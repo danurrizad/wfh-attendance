84e8b2f869a45b256be5d381c2ad10b0dc549327
danurrizad <danurrizad242@gmail.com>
2025-08-11 17:06:21 +0700

    feat: add error message when error on camera device

--- frontend/src/components/face-capture/FaceCapture.jsx
+++ frontend/src/components/face-capture/FaceCapture.jsx
@@ -1,10 +1,15 @@
 // src/FaceCapture.js
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import Button from "../forms/Button";
+import { useToast } from "../../context/ToastifyContext";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faSadTear, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export default function FaceCapture({ imageFile, setImageFile, handleSubmit}) {
   const webcamRef = useRef(null);
+  const { showError } = useToast();
+  const [deviceError, setDeviceError] = useState(false)
 
   const captureImage = () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -15,9 +20,15 @@ export default function FaceCapture({ imageFile, setImageFile, handleSubmit}) {
     setImageFile(null)
   }
 
+  const handleError = (error) => {
+    console.log("Error camera device", error)
+    setDeviceError(true)
+    showError("Can't use camera device over HTTP!")
+  }
+
   return (
     <div>
-        {!imageFile && (
+        {(!imageFile && !deviceError) && (
             <div>
                 <Webcam
                   ref={webcamRef}
@@ -25,9 +36,17 @@ export default function FaceCapture({ imageFile, setImageFile, handleSubmit}) {
                   screenshotFormat="image/jpeg"
                   width={1000}
                   videoConstraints={{ facingMode: "user" }}
+                  onUserMediaError={handleError}
+                  onUserMedia={()=>console.log("ABOSRT")}
                 />
                 <Button className="w-full mt-4" variant="blue" onClick={captureImage}>Capture</Button>
             </div>
+        )}
+        {deviceError && (
+          <div className="flex flex-col items-center justify-center gap-2 p-10">
+            <FontAwesomeIcon icon={faSadTear} className="text-5xl text-gray-300"/>
+            <p className="text-gray-400">Can't open camera device</p>
+          </div>
         )}
       {imageFile && (
         <>
