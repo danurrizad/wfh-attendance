3685a9d03e11e906bd2852122c3c94e2c63c670b
danurrizad <danurrizad242@gmail.com>
2025-08-11 11:11:31 +0700

    delete: unnecessary page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import './App.css'
 import Login from "../src/pages/login/Login"
-import Home from "../src/pages/home/Home"
 import AppLayout from "./layout/AppLayout";
 import Roles from "./pages/master/Roles";
 import Users from "./pages/master/Users";
@@ -17,7 +16,6 @@ function App() {
           <Route path="/login" element={<Login />} />,
           
           <Route element={<AppLayout/>}>
-            <Route path="/home/admin" element={<Home />} />,
             <Route path="/attendances" element={<Attendance />} />,
             <Route path="/master/roles" element={<Roles />} />,
             <Route path="/master/users" element={<Users />} />,
