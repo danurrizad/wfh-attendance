52e8d44b906623935774982333f5f4eaeb6ee4f1
danurrizad <danurrizad242@gmail.com>
2025-08-11 11:14:27 +0700

    feat: add create attendance for clock in and clock out, and logout

--- frontend/src/pages/home/HomeEmployee.jsx
+++ frontend/src/pages/home/HomeEmployee.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../../components/forms/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRightFromBracket, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
@@ -9,25 +9,39 @@ import { Modal } from '../../components/ui/modal/Modal'
 import { Card, CardContent, CardHeader } from '../../components/ui/card/Card'
 import Webcam from "../../components/face-capture/FaceCapture"
 import useAttendanceDataService from '../../services/AttendanceDataService'
+import { Table, TableBody, TableCell, TableRow } from '../../components/ui/table/Table'
 
 const HomeEmployee = () => {
     const { logout } = useAuthService()
-    const { createAttendance } = useAttendanceDataService()
+    const { createAttendanceClockIn, createAttendanceClockOut, checkStatusToday } = useAttendanceDataService()
     const { auth, clearAuth } = useVerify()
-    const [imageFile, setImageFile] = useState(null)
     const navigate = useNavigate()
+    const [dataStatus, setDataStatus] = useState([])
+    const [imageFile, setImageFile] = useState(null)
     const [showModal, setShowModal] = useState({
         type: "clockIn",
         clockIn: false,
         clockOut: false
     })
-    console.log("CURRENT USER ID LOGGED IN: ", auth.userId)
+
+    const fetchStatusToday = async() => {
+        try {
+            const response = await checkStatusToday(auth?.userId)
+            setDataStatus(response?.data?.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(()=>{
+        if(auth.userId !== null){
+            fetchStatusToday()
+        }
+    }, [auth])
 
     const handleLogout = async() => {
         try {
             await logout()
-            localStorage.setItem("accessToken", "")
-            localStorage.setItem("refreshToken", "")
             clearAuth()
             navigate("/login")
         } catch (error) {
@@ -44,6 +58,7 @@ const HomeEmployee = () => {
     }
 
     const handleCloseModal = (type) => {
+        setImageFile(null)
         setShowModal({
             ...showModal,
             [type]: false
@@ -56,7 +71,12 @@ const HomeEmployee = () => {
             const formData = new FormData()
             formData.append("userId", auth.userId)
             formData.append("imageFile", blob)
-            await createAttendance(formData)
+            if(showModal.type === "clockIn"){
+                await createAttendanceClockIn(formData)
+            }else if(showModal.type === "clockOut"){
+                await createAttendanceClockOut(formData)
+            }
+            fetchStatusToday()
             handleCloseModal(showModal.type)
             setImageFile(null)
         } catch (error) {
@@ -88,24 +108,70 @@ const HomeEmployee = () => {
         <h1 className='text-3xl font-bold'>WFH Attendance System</h1>
         <h3 className='text-md font-semibold'>Welcome to your Work-From-Home attendance system</h3>
 
-        <div className='mt-4 w-[200px]'>
-            <Button onClick={()=>handleOpenModal('clockIn')} variant='blue' className='w-full flex justify-start'>
-                <FontAwesomeIcon icon={faArrowRightToBracket}/>
-                <p>Clock In</p>
-            </Button>
-        </div>
-        <div className='mt-4 w-[200px]'>
-            <Button onClick={()=>handleOpenModal('clockOut')} variant='yellow' className='w-full flex justify-start'>
-                <FontAwesomeIcon icon={faArrowRightFromBracket}/>
-                <p>Clock Out</p>
-            </Button>
-        </div>
-        <div className='mt-4 w-[200px]'>
-            <Button variant='red' onClick={handleLogout} className='w-full flex justify-start'>
-                <FontAwesomeIcon icon={faArrowRightToBracket}/>
-                <p>Logout from Account</p>
-            </Button>
-        </div>
+        <Card className='mt-10'>
+            <CardContent className='w-[400px]'>
+                <div className='flex flex-col'>
+                    <h1 className='font-bold'>Attendance</h1>
+                    <Table className="shadow-none">
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="border-none">Name</TableCell>
+                                <TableCell className="border-none">:</TableCell>
+                                <TableCell className="border-none">{auth.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="border-none">Email</TableCell>
+                                <TableCell className="border-none">:</TableCell>
+                                <TableCell className="border-none">{auth.email}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="border-none">Status</TableCell>
+                                <TableCell className="border-none">:</TableCell>
+                                <TableCell className="border-none">
+                                    <p className={`${
+                                        dataStatus.status === "NOT CLOCKED IN" 
+                                            ?  "bg-red-200 text-red-700" 
+                                            : dataStatus.status === "CLOCKED IN"
+                                                ? "bg-green-200 text-green-700"
+                                                : "bg-yellow-200 text-yellow-700"
+                                        } rounded-full px-4 py-1 font-semibold text-center`}>
+                                        {dataStatus.status}
+                                    </p>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="border-none">Clock In Time</TableCell>
+                                <TableCell className="border-none">:</TableCell>
+                                <TableCell className="border-none">{dataStatus.clockInDate}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className="border-none">Clock Out Time</TableCell>
+                                <TableCell className="border-none">:</TableCell>
+                                <TableCell className="border-none">{dataStatus.clockOutDate}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                <div className='mt-4 w-full'>
+                    <Button disabled={dataStatus.status === "CLOCKED IN" || dataStatus.status === "CLOCKED OUT"} onClick={()=>handleOpenModal('clockIn')} variant='blue' className='w-full flex justify-center'>
+                        <FontAwesomeIcon icon={faArrowRightToBracket}/>
+                        <p>Clock In</p>
+                    </Button>
+                </div>
+                <div className='mt-4 w-full'>
+                    <Button disabled={dataStatus.status === "CLOCKED OUT"} onClick={()=>handleOpenModal('clockOut')} variant='yellow' className='w-full flex justify-center'>
+                        <FontAwesomeIcon icon={faArrowRightFromBracket}/>
+                        <p>Clock Out</p>
+                    </Button>
+                </div>
+                <div className='mt-4 w-full'>
+                    <Button variant='red' onClick={handleLogout} className='w-full flex justify-center'>
+                        <FontAwesomeIcon icon={faArrowRightToBracket}/>
+                        <p>Logout from Account</p>
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
     </div>
   )
 }
